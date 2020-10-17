69cb39ab9fa7b37b983191a24b9ce0d6054842af
Bogdan <bheva95@gmail.com>
2020-10-17 17:14:16 +0300

    add project

--- src/js/script.js
+++ src/js/script.js
@@ -7,13 +7,121 @@ $(document).ready(function () {
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
 		responsive: [
 			{
-				breakpoint: 768,
+				breakpoint: 991,
 				settings: {
 					dots: true,
 					arrows: false,
 				}
 			},
-		]	
-		
+		]
+
 	});
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function () {
+		$(this)
+			.addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+	});
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content--active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list--active');
+			});
+		});
+	};
+
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+
+	// Modal 
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+		$('body').toggleClass('no-scroll');
+	});
+
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+		$('body').toggleClass('no-scroll');
+	});
+
+	$('.button--mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+			$('body').toggleClass('no-scroll');
+		});
+	});
+
+
+	function validateForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста введите свое имя",
+					minlength: jQuery.validator.format("Введите {0} символа!")
+				},
+				phone: "Пожалуйста введите номер телефона",
+				email: {
+					required: "Пожалуйста введите свой email",
+					email: "Неправильно введен адрес почты name@domain.com"
+				}
+			}
+
+		});
+	};
+
+	validateForms('#consultation-form');
+	validateForms('#consultation form');
+	validateForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	// =================
+
+	$('form').submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: 'POST',
+			url: 'mailer/smart.php',
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find('input').val('');
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
+	// ======================
+
+	// Smooth scroll
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#up']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
 });
